36d1c3f179a0b20ed3464e07a485e111770d02e3
Marcquez Tookes <marcquez@Marcquezs-MacBook-Pro.local>
2025-05-25 23:40:06 -0500

    swagger.yaml created

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,18 @@ from app.blueprints.employees import employee_bp
 from app.blueprints.cars import cars_bp
 from app.blueprints.Service_Ticket import serviceTicket_bp
 from app.blueprints.service_type import serviceType_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Mechanic API"
+    }
+)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -22,5 +34,6 @@ def create_app(config_name):
     app.register_blueprint(cars_bp, url_prefix='/cars')
     app.register_blueprint(serviceTicket_bp, url_prefix='/tickets')
     app.register_blueprint(serviceType_bp, url_prefix='/service_types')
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
     
     return app
