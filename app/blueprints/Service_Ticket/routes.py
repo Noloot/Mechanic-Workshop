d6a84e8a6717e6f6b857f256da41aaecb901a489
Marcquez Tookes <marcquez@Marcquezs-MacBook-Pro.local>
2025-05-28 18:20:03 -0500

    Updated Service Ticket PUT to match POST

--- app/blueprints/Service_Ticket/routes.py
+++ app/blueprints/Service_Ticket/routes.py
@@ -11,8 +11,8 @@ from app.utils.util import admin_required
 @limiter.limit("15 per hour")
 @admin_required
 def add_ticket():
-    data = request.get_json()
     
+    data = request.get_json()
     service_type_ids = data.pop("service_type_ids", [])
     
     try:
@@ -120,10 +120,26 @@ def update_ticket(id):
     if not ticket:
         return jsonify({'message': 'Ticket not found'}), 404
     
+    data = request.get_json()
+    service_type_ids = data.pop("service_ticket_ids", [])
+    
     try:
-        ticket = service_ticket_schema.load(request.json, instance=ticket)
+        ticket = service_ticket_create_schema.load(request.json, instance=ticket)
     except ValidationError as e:
         return jsonify(e.messages), 400
+    
+    if service_type_ids:
+        service_types = db.session.query(ServiceType).filter(ServiceType.id.in_(service_type_ids)).all()
+        found_ids = {s.id for s in service_types}
+        requested_ids = set(service_type_ids)
+        missing_ids = requested_ids - found_ids
+        if missing_ids:
+            return jsonify({
+                'message': 'Invalid service_type_ids',
+                'invalid_ids': list(missing_ids)
+            }), 400
+            
+        ticket.services = service_types
     
     db.session.commit()
     return service_ticket_schema.jsonify(ticket), 200
