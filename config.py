9629b5bf86ec02e0e0763393c874a02eed9993e1
Marcquez Tookes <marcquez@Marcquezs-MacBook-Pro.local>
2025-05-30 23:57:04 -0500

    Config fixed

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class TestingConfig:
     CACHE_TYPE = 'SimpleCache'
 
 class ProductionConfig:
-    SQLALCHEMY_DATA_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
     CACHE_TYPE = "SimpleCache"
     
